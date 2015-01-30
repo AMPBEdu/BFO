edf16be3ea8c341be088bef14763350a070f0609
Gratin <andrew.bueide2018@geringschools.net>
2015-01-30 06:55:33 -0700

    Finished textures

--- src/renderEngine/Renderer.java
+++ src/renderEngine/Renderer.java
@@ -1,6 +1,7 @@
 package renderEngine;
 
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL13;
 import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL30;
 
@@ -17,10 +18,22 @@ public class Renderer {
 	public void render(TexturedModel texturedModel){
 		RawModel model = texturedModel.getRawModel();
 		GL30.glBindVertexArray(model.getVaoID());
-		GL20.glEnableVertexAttribArray(0);
+		enableVAO();
+		GL13.glActiveTexture(GL13.GL_TEXTURE0);
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, texturedModel.getTexture().getID());
 		GL11.glDrawElements(GL11.GL_TRIANGLES, model.getVertexCount(), GL11.GL_UNSIGNED_INT, 0);
-		GL20.glDisableVertexAttribArray(0);
+		disableVAO();
 		GL30.glBindVertexArray(0);
+	}
+	
+	public void enableVAO(){
+		GL20.glEnableVertexAttribArray(0);
+		GL20.glEnableVertexAttribArray(1);
+	}
+	
+	public void disableVAO(){
+		GL20.glDisableVertexAttribArray(0);
+		GL20.glDisableVertexAttribArray(1);
 	}
 	
 }
